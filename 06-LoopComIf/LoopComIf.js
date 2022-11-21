13f756e01b05074fd96127f74bb692938d21b2be
Rene Alves <meugameplay@outlook.com>
2022-11-21 13:36:50 -0300

    LoopComIf Corrigido
    O código foi corrigido e agora o loop está funcionando corretamente

--- 06-LoopComIf/LoopComIf.js
+++ 06-LoopComIf/LoopComIf.js
@@ -1,1 +1,16 @@
+const prompt = require("prompt-sync")();
+
 //Criar um loop com ifs
+
+var contador = 0
+var numero = prompt("Digite um número:");
+// é menor         < 3
+// é igual a 3    == 3
+fazerloop();
+function fazerloop() {
+    if (contador < numero){
+        contador++;
+        console.log(contador);
+        fazerloop();
+    }
+}
